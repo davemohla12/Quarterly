83f367c8a36f3e71a4d22eb3db5b7915195fe610
davemohla12 <davemohla@gmail.com>
2025-07-21 10:33:23 +0800

    Reverted vite config back to simple version

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,6 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	build: {
-		minify: 'terser',
-		terserOptions: {
-			compress: {
-				drop_console: true,
-				drop_debugger: true
-			}
-		}
-	},
 	test: {
     globals: true,       
     environment: 'node'
