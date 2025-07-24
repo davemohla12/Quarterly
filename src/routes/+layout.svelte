5ed2be727349152d390d5801a63254b5359f0132
davemohla12 <davemohla@gmail.com>
2025-07-24 10:06:55 +0800

    Added updated Meta Pixel

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -47,7 +47,7 @@
   
   <svelte:head>
     <!-- Meta Pixel Code -->
-      <script>
+    <script>
       !function(f,b,e,v,n,t,s)
       {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
       n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -56,12 +56,13 @@
       t.src=v;s=b.getElementsByTagName(e)[0];
       s.parentNode.insertBefore(t,s)}(window, document,'script',
       'https://connect.facebook.net/en_US/fbevents.js');
-      fbq('init', '759517346727533');
+      fbq('init', '1153115809991171');
       fbq('track', 'PageView');
       </script>
       <noscript><img height="1" width="1" style="display:none" alt="Meta Pixel"
-      src="https://www.facebook.com/tr?id=759517346727533&ev=PageView&noscript=1"
-      /></noscript>
+      src="https://www.facebook.com/tr?id=1153115809991171&ev=PageView&noscript=1"
+      />
+    </noscript>
     <!-- End Meta Pixel Code -->
   </svelte:head>
   
