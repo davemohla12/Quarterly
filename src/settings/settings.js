c82cbdf1cc7ec7262668d21bcdf04195d8faa08d
davemohla12 <davemohla@gmail.com>
2025-07-25 12:54:37 +0800

    Turned on coming soon

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -40,6 +40,6 @@ const today = dayjs('2026-12-31')
 
 const showCheckout = false
 const showReferral = false
-const showComingSoon = false
+const showComingSoon = true
 
 export { currentTaxYear, currentTaxQuarter, q1DueDate, q2DueDate, q3DueDate, q4DueDate, price, priceId, showCheckout, showReferral, adminPasscode, showComingSoon, today }
